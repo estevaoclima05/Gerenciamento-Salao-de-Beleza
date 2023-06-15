e82ca29276dcecb9d631680943e1e34ed825d001
Estevão Lima <estevaoclima05@gmail.com>
2023-06-15 12:45:17 -0300

    Criando Classes

--- SalaoDeBeleza/src/main/java/com/mycompany/salaodebeleza/SalaoDeBeleza.java
+++ SalaoDeBeleza/src/main/java/com/mycompany/salaodebeleza/SalaoDeBeleza.java
@@ -11,6 +11,6 @@ package com.mycompany.salaodebeleza;
 public class SalaoDeBeleza {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
     }
 }
