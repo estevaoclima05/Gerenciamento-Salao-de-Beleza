c41f825027f4a8ea311177690e01a83f38955028
fellipealvess <pi_pinho@hotmail.com>
2023-06-25 15:38:25 -0300

    Implementando lógica do controlador.

--- SalaoDeBeleza/src/main/java/controller/LoginController.java
+++ SalaoDeBeleza/src/main/java/controller/LoginController.java
@@ -4,10 +4,25 @@
  */
 package controller;
 
+import view.LoginView;
+
 /**
  *
  * @author estev
  */
 public class LoginController {
+    
+    private final LoginView view;
+
+    public LoginController(LoginView view) {
+        this.view = view;
+    }
+    
+    public void FizTarefa(){
+        System.out.println("Busquei no banco de dados.");
+        
+        this.view.exibeMensagem("Executei o fiz tarefa.");
+    }
+    
     
 }
