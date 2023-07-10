08cfb70f788f70b97ed06e2fffa33c57b5ae59f6
fellipealvess <pi_pinho@hotmail.com>
2023-07-10 20:49:18 -0300

    Trocando Formatos do Pacote Model.

--- SalaoDeBeleza/src/main/java/model/Cliente.java
+++ SalaoDeBeleza/src/main/java/model/Cliente.java
@@ -1,42 +1,98 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
 /**
  *
- * @author isats
+ * @author estev
  */
-public class Cliente extends Pessoa {
+public class Cliente {
     
-    protected String alergia;
-    protected String endereco;
+    
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private String whatsapp;
+    private String alergia;
 
-    public Cliente(String alergia, String endereco, int id, String nome, String telefone, String email) {
-        super(id, nome, telefone, email);
-        this.alergia = alergia;
+    public Cliente(int id ,String nome, String email, String telefone, String endereco, String whatsapp, String alergia) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
         this.endereco = endereco;
+        this.whatsapp = whatsapp;
+        this.alergia = alergia;
     }
-
-   
-
-    public String getAlergia() {
-        return alergia;
-    }
-
-    public void setAlergia(String alergia) {
+    
+    public Cliente(String nome, String email, String telefone, String endereco, String whatsapp, String alergia) {
+        
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.whatsapp = whatsapp;
         this.alergia = alergia;
     }
 
-    public String getEndereco() {
+    public int getIdC() {
+        return id;
+    }
+
+    public void setIdC(int id) {
+        this.id = id;
+    }
+
+    
+
+    public String getNomeC() {
+        return nome;
+    }
+
+    public void setNomeC(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmailC() {
+        return email;
+    }
+
+    public void setEmailC(String email) {
+        this.email = email;
+    }
+
+    public String getTelefoneC() {
+        return telefone;
+    }
+
+    public void setTelefoneC(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEnderecoC() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
+    public void setEnderecoC(String endereco) {
         this.endereco = endereco;
     }
 
+    public String getWhatsappC() {
+        return whatsapp;
+    }
+
+    public void setWhatsappC(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getAlergiaC() {
+        return alergia;
+    }
+
+    public void setAlergiaC(String alergia) {
+        this.alergia = alergia;
+    }
     
     
     
