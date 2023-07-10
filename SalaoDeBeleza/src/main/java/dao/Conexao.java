96f0ed19559d9918a0e2f1eb21d2572693658149
fellipealvess <pi_pinho@hotmail.com>
2023-07-10 20:46:46 -0300

    Alterando Forma de Conexão com o Banco de Dados.

--- SalaoDeBeleza/src/main/java/dao/Conexao.java
+++ SalaoDeBeleza/src/main/java/dao/Conexao.java
@@ -1,23 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- *
- * @author estev
- */
+
 public class Conexao {
     
-    public Connection getConnection() throws SQLException{
-        Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "lima12345");
-        return conexao;
-        
-    }
+    private static final String DRIVE = "org.postgresql.Driver";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "lima12345";
     
+    
+    public static Connection getConnection() {
+        
+        try {
+            Class.forName(DRIVE);
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        }  catch (ClassNotFoundException | SQLException ErroSql) {
+            throw new RuntimeException("ERRO AO CONECTAR. " + ErroSql);
+    }
+   
 }
+
+    public static void closeConnection(Connection conexao) {
+        try {
+            if(conexao != null)conexao.close();
+        } catch (SQLException ErroSql) {
+            throw new RuntimeException("ERRO AO FECHAR CONEXAO " + ErroSql);
+        }
+    } 
+    
+     public static void closeConnection(Connection conexao, PreparedStatement pstm) {
+         closeConnection(conexao);
+         
+         try {
+            if(pstm != null)pstm.close();
+        } catch (SQLException ErroSql) {
+            throw new RuntimeException("ERRO AO FECHAr PREPAREDS " + ErroSql);
+        }
+    }
+     
+     public static void closeConnection(Connection conexao, PreparedStatement pstm, ResultSet rs) {
+         closeConnection(conexao, pstm);
+         
+         try {
+            if(rs != null)rs.close();
+        } catch (SQLException ErroSql) {
+            throw new RuntimeException("ERRO AO FECHAr PREPAREDS " + ErroSql);
+        }
+     }}
+    
