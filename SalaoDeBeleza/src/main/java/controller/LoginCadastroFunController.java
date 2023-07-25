9332eb372fa7a12f8ec72ecd7c69536a5fddaa6f
Gabrieljvs <gabrieljsouzaa03@gmail.com>
2023-07-25 17:26:18 -0300

    Alterando viabilidade na Classe de LoginCadastroFunController

--- SalaoDeBeleza/src/main/java/controller/LoginCadastroFunController.java
+++ SalaoDeBeleza/src/main/java/controller/LoginCadastroFunController.java
@@ -1,88 +1,50 @@
 package controller;
 
-/**
- *
- * @author estev
- */
-import dao.Conexao;
 import dao.FuncionarioDAO;
-import java.sql.Connection;
+
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import model.Funcionario;
 import view.LoginView;
-import view.MenuView;
 
-/**
- *
- * @author estev
- */
+
+
 public class LoginCadastroFunController {
-    
-    private final LoginView view;
 
-    public LoginCadastroFunController(LoginView view) {
-      this.view = view;
-    }
+    public void cadastrarFunc(String nome, String telefone, String email, String senha) throws Exception {
 
-       public void autenticar() throws SQLException {
-        
-        // Buscar um usuario da view
-        
-        String email = view.getjTextFieldUserEmail().getText();
-        String senha = view.getjPasswordFieldUserSenha().getText();
-        Funcionario usuarioAutenticar = new Funcionario(email, senha);
-        
-        // Verificar se existe um no BD
-        
-        
-        FuncionarioDAO usuarioDao = new FuncionarioDAO();
-        
-        boolean existe = usuarioDao.existeNoBancoPorUsuarioESenha(usuarioAutenticar);
-        
-        
-        // Se existir direcionar para menu, se n msg error.
-        if(existe) {
-            MenuView telamenu = new MenuView();
-            this.view.dispose();
-            telamenu.setVisible(true);
-        } else{
-            JOptionPane.showMessageDialog(view, "Usuario ou senha incorretos, tente novamente.");
-        }
-        
-    }
-    
-    
-    public void cadastrarFunc() {
-        
-        String nome = view.getjTextFieldNome().getText();
-        
-        String telefone = view.getjTextFieldTelefone().getText();
-        
-        String email = view.getjTextFieldEmail().getText();
+        Funcionario obj = new Funcionario();
 
-        String senha = view.getjPasswordFieldSenha().getText();
-        
-       
-        Funcionario funcionarioCadastrar = new Funcionario(nome, telefone, email, senha);
+        obj.setNome(nome);
+        obj.setTelefone(telefone);
+        obj.setEmail(email);
+        obj.setSenha(senha);
+
         try {
-            //Connection conexao = Conexao.getConnection();
-            FuncionarioDAO usuarioDao = new FuncionarioDAO(); 
-            usuarioDao.insert(funcionarioCadastrar);
-            
-            view.getjTextFieldNome().setText("");
-            view.getjTextFieldEmail().setText("");
-            view.getjTextFieldTelefone().setText("");
-            view.getjPasswordFieldSenha().setText("");
-            
-            JOptionPane.showMessageDialog(null, "Cadastro feito com sucesso");
-            
+            FuncionarioDAO dao = new FuncionarioDAO();
+            dao.insert(obj);
         } catch (SQLException ex) {
             Logger.getLogger(LoginView.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
-    
-    
+
+    public boolean autenticar(String email, String senha) throws Exception {
+
+        Funcionario obj = new Funcionario();
+
+        obj.setEmail(email);
+        obj.setSenha(senha);
+
+        FuncionarioDAO dao = new FuncionarioDAO();
+
+        if (dao.existeNoBancoPorUsuarioESenha(obj)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
