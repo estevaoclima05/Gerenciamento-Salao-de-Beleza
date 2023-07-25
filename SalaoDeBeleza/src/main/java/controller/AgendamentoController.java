3abbb18bc71c6a73b5a7a4140f8a068d153f4ca5
Isabele Lemos <lemosisa077@gmail.com>
2023-07-25 17:41:47 -0300

    Alterando viabilidade na Classe de AgendamentoController

--- SalaoDeBeleza/src/main/java/controller/AgendamentoController.java
+++ SalaoDeBeleza/src/main/java/controller/AgendamentoController.java
@@ -1,137 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
+
+
 import dao.AgendamentoDAO;
-import dao.ClienteDAO;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import model.Agendamento;
-import model.Cliente;
 import view.AgendamentoTela;
-import view.CadastroClienteTela;
-import view.LoginView;
 
-/**
- *
- * @author isats
- */
+
+
 public class AgendamentoController {
-    private final AgendamentoTela view;
 
-    public AgendamentoController(AgendamentoTela view) {
-      this.view = view;
-    }
-    
-    
-    public void resetarAgendamento() {
-        
-        view.getjTextFieldIdA().setText("");
-        view.getjTextFieldClienteA().setText("");
-        view.getjTextFieldServicoA().setText("");
-        view.getjTextFieldPrecoA().setText("");
-        view.getjTextFieldHorarioA().setText("");
-        
-        
-    }
-    
-    
-    public void agendarCliente() {
-        
-      
-        
-        
-        
-        String cliente = view.getjTextFieldClienteA().getText();
-        
-        String servico = view.getjTextFieldServicoA().getText();
-        
-        Double preco = Double.parseDouble( view.getjTextFieldPrecoA().getText());
-        
-        String horario = view.getjTextFieldHorarioA().getText();
-        
-       
+    public void agendar(String cliente, String servico, Double preco, String data, String horario) throws Exception {
 
-        Agendamento clienteAgendar = new Agendamento(cliente, servico, preco, horario);
-        
+        Agendamento obj = new Agendamento();
+
+        obj.setCliente(cliente);
+
+        obj.setServico(servico);
+        obj.setPreco(preco);
+        obj.setHorario(horario);
+        obj.setData(data);
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+
         try {
-            
-            AgendamentoDAO agendamentoDao = new AgendamentoDAO();
-            agendamentoDao.inserir(clienteAgendar);
-            
-            JOptionPane.showMessageDialog(null, "Agendamento feito com sucesso");
+            dao.inserir(obj);
         } catch (SQLException ex) {
-            Logger.getLogger(AgendamentoController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AgendamentoTela.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
     }
-    
-    public void atualizarAgendamento() {
-        
-                
-        
-        int id = Integer.parseInt(view.getjTextFieldIdA().getText());
-        
-        String cliente = view.getjTextFieldClienteA().getText();
-        
-        String servico = view.getjTextFieldServicoA().getText();
-        
-        Double preco = Double.parseDouble( view.getjTextFieldPrecoA().getText());
-        
-        String horario = view.getjTextFieldHorarioA().getText();
-        
-        Agendamento agendamentoAtualizar = new Agendamento( id, cliente, servico, preco, horario);
+
+    public void alterar(Object id, String cliente, String servico, Double preco, String data, String horario) throws Exception {
+
+        Agendamento obj = new Agendamento();
+
+        obj.setId((int) id);
+        obj.setCliente(cliente);
+
+        obj.setServico(servico);
+        obj.setPreco(preco);
+        obj.setHorario(horario);
+        obj.setData(data);
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+
         try {
-            
-            AgendamentoDAO agendamentoDAO = new AgendamentoDAO();
-            agendamentoDAO.update(agendamentoAtualizar);
-            
-           
-            
-            JOptionPane.showMessageDialog(null, "Alterado com sucesso");
-            
-        } 
-        catch (SQLException ex) {
-            Logger.getLogger(LoginView.class.getName()).log(Level.SEVERE, null, ex);
+            dao.update(obj);
+        } catch (SQLException ex) {
+            Logger.getLogger(AgendamentoTela.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
 
-    
-    public void deletarAgendamento() {
-        
-        int id = Integer.parseInt(view.getjTextFieldIdA().getText());
-        
-        String cliente = view.getjTextFieldClienteA().getText();
-        
-        String servico = view.getjTextFieldServicoA().getText();
-        
-        Double preco = Double.parseDouble( view.getjTextFieldPrecoA().getText());
-        
-        String horario = view.getjTextFieldHorarioA().getText();
+    public void apagar(Object id) throws Exception {
 
-        
-        Agendamento agendamentoDeletar = new Agendamento(id, cliente, servico, preco, horario);
+        Agendamento obj = new Agendamento();
+
+        obj.setId((int) id);
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+
         try {
-            
-            AgendamentoDAO agendamentoDAO = new AgendamentoDAO();
-            agendamentoDAO.delete(agendamentoDeletar);
-           
-           
-            
-            JOptionPane.showMessageDialog(null, "Deletado com sucesso");
-            
+            dao.delete(obj);
+        } catch (SQLException ex) {
+            Logger.getLogger(AgendamentoTela.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch (SQLException ex) {
-            Logger.getLogger(LoginView.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
     }
-    
-} 
 
+}
