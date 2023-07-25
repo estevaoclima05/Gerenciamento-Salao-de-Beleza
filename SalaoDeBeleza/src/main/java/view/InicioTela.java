09aa66848c673f73f3f2104f688633889802a663
Gabrieljvs <gabrieljsouzaa03@gmail.com>
2023-07-25 17:34:46 -0300

    Alteralção na interface grafica e automatização com o controller

--- SalaoDeBeleza/src/main/java/view/InicioTela.java
+++ SalaoDeBeleza/src/main/java/view/InicioTela.java
@@ -1,15 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
- */
+
 package view;
 
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 
-/**
- *
- * @author estev
- */
+
+
 public class InicioTela extends javax.swing.JInternalFrame {
 
     /**
