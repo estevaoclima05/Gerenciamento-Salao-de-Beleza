c42c9e0110941670f50b5967facb9c03895daf8e
Matheus27G <mateusgustavooliveira@hotmail.com>
2023-07-27 15:27:42 -0300

    Criando classe de CaixaController e viabilizando com o banco de dados.

---
+++ SalaoDeBeleza/src/main/java/controller/CaixaController.java
@@ -0,0 +1,51 @@
+
+package controller;
+
+import dao.AgendamentoDAO;
+import dao.CaixaDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import model.Agendamento;
+import view.AgendamentoTela;
+import model.Caixa;
+
+
+public class CaixaController {
+    
+    
+    public void feito(Object id, String cliente, String data, Double preco) throws Exception {
+
+        Caixa obj = new Caixa();
+        Agendamento objA = new Agendamento();
+        objA.setId((int) id);
+        
+        obj.setIdpc((int) id);
+        obj.setClientepc(cliente);
+        obj.setDatapc(data);
+        obj.setPrecopc(preco);
+      
+        CaixaDAO dao = new CaixaDAO();
+        AgendamentoDAO daoAgen = new AgendamentoDAO();
+
+        try {
+            dao.inserir(obj);
+          try {
+            daoAgen.delete(objA);
+          
+        } catch (SQLException ex) {
+            Logger.getLogger(AgendamentoTela.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        } catch (SQLException ex) {
+            Logger.getLogger(AgendamentoTela.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+ 
+    
+   
+   
+
+    
+}
