9512a9fa6dc4ca94c2ca6252a52e0b6e746273cc
Estevão Lima <estevaoclima05@gmail.com>
2023-08-01 20:42:29 -0300

    Testes Unitarios

---
+++ SalaoDeBeleza/src/test/java/controller/AgendamentoControllerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author estev
+ */
+public class AgendamentoControllerTest {
+    
+    public AgendamentoControllerTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of agendar method, of class AgendamentoController.
+     */
+    @Test
+    public void testAgendar() throws Exception {
+        System.out.println("agendar");
+      
+    }
+
+    /**
+     * Test of alterar method, of class AgendamentoController.
+     */
+    @Test
+    public void testAlterar() throws Exception {
+        System.out.println("alterar");
+        
+    }
+
+    /**
+     * Test of apagar method, of class AgendamentoController.
+     */
+    @Test
+    public void testApagar() throws Exception {
+        System.out.println("apagar");
+        
+    }
+    
+}
